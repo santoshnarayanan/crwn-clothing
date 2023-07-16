b43b1e4cd4f2eb5dad8b063de8b4677118014cc0
Santosh Narayanan <santosh33@gmail.com>
2023-07-17 00:36:24 +0200

    Removed code for GoogleRedirect to keep SignIn()

--- src/routes/home/sign-in/sign-in.component.jsx
+++ src/routes/home/sign-in/sign-in.component.jsx
@@ -1,26 +1,11 @@
-/* import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth"; */
 
 import {
     auth,
     signInWithGooglePopup,
-    // signInWithGoogleRedirect,
     createUserDocumentFromAuth
 } from "../../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-   /*  useEffect(() => {
-        const responseData = async () =>{
-            const data = await getRedirectResult(auth);
-            if(data){
-                const userDocRef = await  createUserDocumentFromAuth(data.user);
-            }
-            //console.log(data);
-        } 
-        // * calling method
-        responseData().catch(console.error);
-    }, []); */
-
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -31,7 +16,6 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign with Google Popup</button>
-            {/* <button onClick={signInWithGoogleRedirect}>Sign with Google Redirect</button> */}
         </div>
     );
 };
